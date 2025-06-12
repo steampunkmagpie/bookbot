9c12a67896be63d301504bb449ed694083fcf7ab
steampunkmagpie <ajpan1@gmail.com>
2025-06-12 00:19:43 -0400

    Added Character Count function to bookbot

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from stats import word_count
 
+from stats import char_count
+
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
@@ -8,6 +10,8 @@ def get_book_text(filepath):
 def main():
     book = get_book_text("books/frankenstein.txt")
     num_words = word_count(book)
+    char=char_count(book)
     print(f"{num_words} words found in the document")
+    print(char)
 
 main()
--- stats.py
+++ stats.py
@@ -5,3 +5,15 @@ def word_count(book):
         count += 1
         num_words = count
     return num_words
+
+def char_count(book):
+    char = {}
+    book=str.lower(book)
+    words = book.split()
+    for word in words:
+        for i in word:
+            if i in char:
+                char[i] +=1
+            else:
+                char[i]=1
+    return char
