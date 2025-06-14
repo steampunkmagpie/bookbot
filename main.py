7193a023bf1b2e15c2508f79438801666da4e0f5
steampunkmagpie <ajpan1@gmail.com>
2025-06-13 22:20:51 -0400

    Finished Bookbot!

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+import sys
+
 from stats import word_count
 
 from stats import char_count
 
 from stats import sorted_list
 
-file_path = "books/frankenstein.txt"
+if len(sys.argv) <= 1:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+    
+
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -12,12 +18,12 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    book = get_book_text(file_path)
+    book = get_book_text(sys.argv[1])
     num_words = word_count(book)
     char=char_count(book)
     sorted=sorted_list(char)
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {file_path}...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
